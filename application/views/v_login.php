bf6c696b36ef1f23c6feb22d34e31496e9e32dde
Embun Duta Laksmana <embun20ti@mahasiswa.pcr.ac.id>
2021-07-05 14:12:47 +0700

    konfig2

---
+++ application/views/v_login.php
@@ -0,0 +1,40 @@
+﻿<!DOCTYPE html>
+<html>
+
+<head>
+    
+</head>
+
+<body class="login-page">
+    <div class="login-box">
+        <div class="card">
+            <div class="body">
+                <form id="sign_in" method="POST" action="<?php echo base_url('login/aksi_login'); ?>">
+                    <div class="msg">Selamat Datang</div>
+                    <div class="input-group">
+                        <span class="input-group-addon">
+                            <i class="material-icons"></i>
+                        </span>
+                        <div class="form-line">
+                            <input type="text" class="form-control" name="username" placeholder="Username" required autofocus>
+                        </div>
+                    </div>
+                    <div class="input-group">
+                        <span class="input-group-addon">
+                            <i class="material-icons">lock</i>
+                        </span>
+                        <div class="form-line">
+                            <input type="password" class="form-control" name="password" placeholder="Password" required>
+                        </div>
+                    </div>
+                        <div class="col-xs-4">
+                            <button class="btn btn-block bg-pink waves-effect" type="submit">Masuk</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
